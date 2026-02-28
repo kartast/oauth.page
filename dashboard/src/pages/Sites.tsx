0702cfb5c32722a0b1cb4ce87026e42872f3640a
karta <kartast@kartas.mac.studio.lan>
2026-02-28 15:14:18 +0800

    ui: 2-column site grid on mobile

--- dashboard/src/pages/Sites.tsx
+++ dashboard/src/pages/Sites.tsx
@@ -164,7 +164,7 @@ export default function Sites() {
           </Link>
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
           {sites.map((site) => (
             <SiteCard key={site.id} site={site} />
           ))}
